4e6afb5062489918870f85514554d5b8ef6aa08c
Khanalshreejan <88266743+Khanalshreejan@users.noreply.github.com>
2021-09-14 02:31:10 -0500

    Add files via upload

--- first_order_approach.py
+++ first_order_approach.py
@@ -23,10 +23,16 @@ from drivingforce import random_force
 
 
 # function that returns dqdt_real and dqdt_imag
-def dQ_dt( q, t, om=4, gam=0.3):
+def dQ_dt(q, tvalues, om, gam):
     
     q_real = q[0]
+    
     q_imag = q[1]
+    
+    # print(q[0], q[1])
+    
+    # print(tvalues)
+    
     dqdt_real = (q_imag * om) - (gam * q_real) 
     
     dqdt_imag = -(q_real * om) -(gam * q_imag) 
@@ -43,33 +49,56 @@ def dQ_dt( q, t, om=4, gam=0.3):
     #want to calculate f((t)
     for j in range(len(random_force)):
         
-        # tj =  random_force[j]['time_impulse']
-        tj =0
-        cj_real =0
-        cj_imag =0
-        if abs(t-tj) > 10*Taucorr :
+        
+        # time_impulses =[]
+        
+        # Cj_real =[]
+        
+        # Cj_imag =[]
+        # time_impulse =[]
+        
+        # diff = np.asarray(t) - np.asarray(tj)
+        
+        if abs(timediff) > (10*Taucorr) :
+            
           continue;  
-        # cj_real = random_force[j]['real']
+        # for i in range (len(random_force[j])):  
+
+        time_impulses = random_force[j][0] #first column passed as time_impulses
         
-        # cj_imag = random_force[j]['imag']
+        # print(time_impulses)
         
-        Psi_j = math.exp(-(t - tj)**2/(2*Taucorr**2))
+        cj_real = random_force[j][1]  #second column passed as cj_real
+        # print(cj_real)
+        cj_imag = random_force[j][2]   #third column passed as cj_imag
+          
+        psi_j = math.exp(-(timediff)**2/(2*Taucorr**2))
         
-        force_real += (cj_real * Psi_j)
+        force_real += (cj_real * psi_j)
         
-        force_imag += (cj_imag * Psi_j)
+        force_imag += (cj_imag * psi_j)
         
+    
+    
+    # time_impulses.append(time_impulse)
+    
+    
    
     
     dqdt_real += -(om * force_imag)
     
     dqdt_imag += (om * force_real )
     
-    equation =[dqdt_real, dqdt_imag]
+  
+    equation = (dqdt_real, dqdt_imag)
     
-    return dqdt_real, dqdt_imag
+    
+    return equation
+ 
 
-q0 = [0.001,0.00002]
+q0 = [0.001, 0.0004]
+
+
 
 lowerb = t2_array[0]
 
@@ -86,7 +115,6 @@ gam=0.3
 # f0=2
 
 # omdrive=3
-
 Qvalues = odeint(dQ_dt, q0, tvalues, args =( om, gam))
 
 Qvalues_real = Qvalues[:,0]
